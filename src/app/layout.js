7900c81c20d1c131b0bbb6161526075d07f5dbec
himanshu7339 <himanshusingh5t5t@gmail.com>
2024-07-26 10:34:47 +0530

    add google tag manager

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import Header from "@/components/common/Header";
 import Footer from "@/components/common/Footer";
 import TopBar from "@/components/common/TopBar";
 import WhatApp from "@/components/common/WhatApp";
-import { GoogleAnalytics } from '@next/third-parties/google'
+import { GoogleTagManager } from '@next/third-parties/google'
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -16,7 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-      <GoogleAnalytics gaId={process.env.GTM} />
+      <GoogleTagManager gtmId={process.env.GTM} />
       <WhatApp/>
         <TopBar />
         <Header />
