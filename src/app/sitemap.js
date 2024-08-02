76263b174d8d6df27aa2cd4163e25a1b539b1230
himanshu7339 <himanshusingh5t5t@gmail.com>
2024-08-03 00:14:25 +0530

    add site map

---
+++ src/app/sitemap.js
@@ -0,0 +1,17 @@
+const BASE_URL = "https://appwisestudios.in";
+export default function sitemap() {
+  return [
+    {
+      url: `${BASE_URL}/about`,
+    },
+    {
+      url: `${BASE_URL}/contact`,
+    },
+    {
+      url: `${BASE_URL}/price`,
+    },
+    {
+      url: `${BASE_URL}/termsandconditions`,
+    },
+  ];
+}
